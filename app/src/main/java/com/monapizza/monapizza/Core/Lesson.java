b290b0db52ef382217449f64628b00bcaaa34f7a
chitai <chitai.vct@gmail.com>
2017-11-11 15:51:00 +0700

    Add Temporary Layout For Views

---
+++ app/src/main/java/com/monapizza/monapizza/Core/Lesson.java
@@ -0,0 +1,8 @@
+package com.monapizza.monapizza.Core;
+
+/**
+ * Created by chita on 11/11/2017.
+ */
+
+public class Lesson {
+}
--- app/src/main/java/com/monapizza/monapizza/UI/LessonsActivity.java
+++ app/src/main/java/com/monapizza/monapizza/UI/LessonsActivity.java
@@ -1,15 +1,33 @@
 package com.monapizza.monapizza.UI;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import com.monapizza.monapizza.Core.Word;
 import com.monapizza.monapizza.R;
 
-public class LessonsActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class LessonsActivity extends AppCompatActivity {
+    ArrayList<Word> mWords;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lessons);
+
+        String s = getIntent().getStringExtra("words");
+        importWords(s);
+
+        LinearLayoutManager layoutManager
+                = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+
+        RecyclerView myList = (RecyclerView) findViewById(R.id.lessons_list);
+        myList.setLayoutManager(layoutManager);
+    }
+
+    protected void importWords(String s) {
+
     }
 }
