999b66c85ce77326b2d80c061ab1c294271439ea
thatvinhton <thatvinh.ton@gmail.com>
2017-11-12 08:20:05 +0700

    Update Lesson class

--- app/src/main/java/com/monapizza/monapizza/Core/Lesson.java
+++ app/src/main/java/com/monapizza/monapizza/Core/Lesson.java
@@ -1,8 +1,47 @@
 package com.monapizza.monapizza.Core;
 
+import java.util.ArrayList;
+
 /**
  * Created by chita on 11/11/2017.
  */
 
 public class Lesson {
+    // category id
+    int m_category;
+
+    // lesson id
+    int m_lesson;
+
+    // lesson name
+    String m_name;
+
+    // list of words in lesson
+    ArrayList<String> m_wordsList;
+
+    private Lesson() {}
+
+    public Lesson(int category, int lesson) {
+        m_category = category;
+        m_lesson = lesson;
+
+        // Chua co database
+        /*
+        m_wordsList = database.getWordInLevel(category, lesson);
+
+        int numberLessonInCategory = database.getNumberOfLesson(category);
+
+        m_name = Integer.toString(lesson) + "/" + Integer.toString(numberLessonInCategory);
+        */
+
+        m_name = "Chua co ten";
+    }
+
+    ArrayList<String> getWordsList() {
+        return m_wordsList;
+    }
+
+    String getName() {
+        return m_name;
+    }
 }
