617249e6df14d29799a4f9488c696704c6f97495
Phuc <nvinhphuc210@gmail.com>
2017-12-02 21:18:12 +0700

    Thêm database

--- app/src/main/java/com/monapizza/monapizza/database/DbHelper.java
+++ app/src/main/java/com/monapizza/monapizza/database/DbHelper.java
@@ -1,19 +1,111 @@
 package com.monapizza.monapizza.database;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+
 import com.monapizza.monapizza.core.Category;
 import com.monapizza.monapizza.core.Word;
 import com.monapizza.monapizza.core.Lesson;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 
 /**
  * Created by chita on 01/11/2017.
  */
 
-public class DbHelper {
+public class DbHelper extends SQLiteOpenHelper {
+
+    private Context context;
+    private String DB_PATH = "data/data/com.monapizza.monapizza";
+    private static String DB_NAME = "database.sqlite";
+    private SQLiteDatabase myDatabase;
+
+    public DbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    private boolean checkDatabase() {
+        boolean checkDb = false;
+        try {
+            String myPath = DB_PATH + DB_NAME;
+            File dbFile = new File(myPath);
+            checkDb = dbFile.exists();
+        }
+        catch (SQLiteException e) {
+            System.out.print("Database doesn't exist");
+        }
+        return checkDb;
+    }
+
+    private void copyDatabase() throws IOException {
+        AssetManager  dirPath = context.getAssets();
+        InputStream myInput = context.getAssets().open(DB_NAME);
+        String outFileName = DB_PATH + DB_NAME;
+        OutputStream myOutput = new FileOutputStream("data/data/com.monapizza.monapizza/database.sqlite");
+        byte[] buffer = new byte[1024];
+        int len;
+        while ((len = myInput.read(buffer)) > 0) {
+            myOutput.write(buffer, 0, len);
+        }
+        myOutput.flush();
+        myOutput.close();
+        myInput.close();
+    }
+
+    public void createDatabase() {
+        this.getReadableDatabase();
+        try {
+            copyDatabase();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public DbHelper(Context context) {
+        super(context, DB_NAME, null, 1);
+        this.context = context;
+        boolean dbExist = checkDatabase();
+        if(!dbExist)
+        {
+            System.out.println("Database doesn't exist!");
+            createDatabase();
+        }
+    }
+
+    public void open() {
+        String myPath = DB_PATH + DB_NAME;
+        myDatabase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READWRITE);
+    }
+
+    public synchronized void close() {
+        myDatabase.close();
+        super.close();
+    }
 
     // Lay danh sach cac tu thuoc level - khoi tao checkpoint
     public ArrayList<Word> getWordInLevel(int level) {
+
+
         return new ArrayList<Word>();
     }
 
